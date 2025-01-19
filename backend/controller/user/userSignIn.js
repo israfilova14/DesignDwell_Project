1a7df90c0c47a28ed1c680dd49a9d2bd0c3045ba
israfilova14 <aynurisrafilova9@gmail.com>
2025-01-19 14:29:15 +0400

    commit

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -33,7 +33,7 @@ async function userSignInController(req, res){
         const tokenOption = {
           httpOnly: true,
           secure: true,
-          sameSite: false
+          sameSite: true
         }
 
         res.cookie("token", token, tokenOption).json({
